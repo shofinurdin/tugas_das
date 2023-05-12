ed0fc568a628861e2ed4be2097376f79bada3c03
shofinurdin <shofinurdin@gmail.com>
2023-05-12 20:01:29 +0700

    commit 5

--- predict.py
+++ predict.py
@@ -14,10 +14,8 @@ def run_predict_app():
     
     st.subheader("Classification Prediction")
     
-
+    model_upload = st.file_uploader('Upload Dataset ', type=['joblib'])
     
-    
-
     st.sidebar.title("Features")
     #Intializing
     sl = st.sidebar.slider(label="Sepal Length (cm)",value=5.2,min_value=0.0, max_value=8.0, step=0.1)
@@ -25,19 +23,22 @@ def run_predict_app():
     pl = st.sidebar.slider(label="Petal Length (cm)",value=4.2,min_value=0.0, max_value=8.0, step=0.1)
     pw = st.sidebar.slider(label="Petal Width (cm)",value=1.2,min_value=0.0, max_value=8.0, step=0.1)
 
-    if st.button("Click Here to Classify"):
-        dfvalues = pd.DataFrame(list(zip([sl],[sw],[pl],[pw])),columns =['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
-        input_variables = np.array(dfvalues[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']])
-        st.write('Input :')
-        data_input=pd.DataFrame(data=input_variables, columns=['Sepal Length','Sepal Width','Petal Length','Petal Width'])
-        st.dataframe(data_input)
-        model = load('model.joblib')
-        prediction = model.predict(input_variables)
-        st.write('Prediction :')
-        st.success(prediction[0])
-        if prediction == 'Iris-setosa':
-            st.image('setosa.png')
-        elif prediction == 'Iris-versicolor':
-            st.image('versicolor.png')
-        elif prediction == 'Iris-virginica':
-            st.image('virginica.png')
+    if model_upload is not None:
+        if st.button("Click Here to Classify"):
+            dfvalues = pd.DataFrame(list(zip([sl],[sw],[pl],[pw])),columns =['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
+            input_variables = np.array(dfvalues[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']])
+            st.write('Input :')
+            data_input=pd.DataFrame(data=input_variables, columns=['Sepal Length','Sepal Width','Petal Length','Petal Width'])
+            st.dataframe(data_input)
+            model = load(model_upload)
+            prediction = model.predict(input_variables)
+            st.write('Prediction :')
+            st.success(prediction[0])
+            if prediction == 'Iris-setosa':
+                st.image('setosa.png')
+            elif prediction == 'Iris-versicolor':
+                st.image('versicolor.png')
+            elif prediction == 'Iris-virginica':
+                st.image('virginica.png')
+    else:
+        st.write('model belum diupload')
